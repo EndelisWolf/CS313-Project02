d1d8d8052fa93b504afcf2ff00f618d0674cea7b
Michael <wil13040@byui.edu>
2019-07-06 19:14:16 -0600

    Worked on Milestone 2 - Edits to Update need to be made

--- app.js
+++ app.js
@@ -1,19 +1,38 @@
 const express = require("express");
 const path = require("path");
+const bodyParser = require("body-parser")
 var app = express();
 
-const { Pool } = require("pg");
+const {
+    Pool
+} = require("pg");
 
 const connectionString = process.env.DATABASE_URL || "postgres://todo_user:remember@localhost:5432/todo";
-const pool = new Pool({connectionString: connectionString});
+const pool = new Pool({
+    connectionString: connectionString
+});
 
 app.set("port", (process.env.PORT || 7000));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(bodyParser.json())
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+
 app.get("/getNote", getNote)
 
-app.listen(app.get("port"), function() {
+app.get("/locatetodo", locatetodo)
+
+app.post("/addNote", addNote)
+
+app.get("/deleteNote", deleteNote)
+
+app.post("/updateNote", updateNote)
+
+app.listen(app.get("port"), function () {
     console.log("Now listening for: ", app.get("port"));
 });
 
@@ -21,13 +40,16 @@ function getNote(req, res) {
     console.log("Retrieving Note"); //error here? note listed below?
 
     var id = req.query.id;
-    console.log("Retrieving id", id);    
+    console.log("Retrieving id", id);
 
-    getNoteFromDb(id, function(error, result) {
+    getNoteFromDb(id, function (error, result) {
         console.log("Retrieved from DB Function with note: ", result)
 
         if (error || result == null) {
-            res.status(500).json({success:false, data: error})
+            res.status(500).json({
+                success: false,
+                data: error
+            })
         } else {
             res.json(result[0]);
         }
@@ -40,7 +62,7 @@ function getNoteFromDb(id, callback) {
     var sql = "SELECT noteentry, duedate FROM list WHERE id =  $1::int"; //may not be correct
     var params = [id];
 
-    pool.query(sql, params, function(err, result) {
+    pool.query(sql, params, function (err, result) {
         if (err) {
             console.log("Error in DB");
             console.log(err);
@@ -48,6 +70,107 @@ function getNoteFromDb(id, callback) {
         }
 
         console.log("DB found result: " + JSON.stringify(result.rows));
+
+        callback(null, result.rows)
+    })
+}
+
+function locatetodo(req, res) {
+    var sql = "SELECT id FROM list";
+    pool.query(sql, function (err, data) {
+        res.json(data.rows)
+    })
+}
+//everything below is milestone 2
+function addNote(req, res) {
+    console.log(req.body)
+
+    var duedate = req.body.duedate;
+    var noteentry = req.body.noteentry;
+
+    var sql = "INSERT INTO list (duedate, noteentry) VALUES ($1, $2)"
+
+    pool.query(sql, [duedate, noteentry], function (err, done) {
+        //done();
+        //res.redirect('/');
+    })
+
+}
+
+function deleteNote(req, res) {
+    console.log("Retrieving note to be deleted")
+
+    var id = req.query.id;
+    console.log("Retrieved note #", id, "to be deleted");
+
+    deleteNoteFromDb(id, function (error) {
+        console.log("Note has been deleted.")
+
+    });
+}
+
+function deleteNoteFromDb(id, callback) {
+    console.log("Note called with id:", id);
+
+    var sql = "DELETE FROM list WHERE id = $1::int";
+    var params = [id];
+
+    pool.query(sql, params, function (err, result) {
+        if (err) {
+            console.log("Error in DB");
+            console.log(err);
+            callback(err, null);
+        }
+
+        console.log("DB found note to be deleted.");
+
+        callback(null)
+    })
+}
+
+function updateNote(req, res) {
+    console.log("Retrieving note that needs updating.")
+
+    
+    var id = req.query.id;
+    //var sql = "INSERT INTO list (duedate, noteentry) VALUES ($1, $2)"
+
+    console.log("Note #", id, "is being updated.")
+
+    updateNoteFromDb(id, function(error, result) {
+        console.log("Retrieved from DB Note to be updated:", result)
+        if (error || result == null) {
+            res.status(500).json({
+                success: false,
+                data: error
+            })
+        } else {
+            res.json(result[0]);
+        }
+    })
+
+    //pool.query(sql, [duedate, noteentry], function (err, done) {
+        //done();
+        //res.redirect('/');
+    //})
+}
+
+function updateNoteFromDb(id, req, callback) {
+    console.log("Note called with id:", id);
+
+    var noteentry = req.body.noteentry;
+
+    var sql = "UPDATE list SET noteentry='noteentry' where id = $1::int";
+    var params = [id, noteentry];
+
+    pool.query(sql, params, function (err, result) {
+        if (err) {
+            console.log("Error in DB");
+            console.log(err);
+            callback(err, null);
+        }
+
+        console.log("Note in DB updated.");
 
         callback(null, result.rows)
     })
--- public/index.js
+++ public/index.js
@@ -1,5 +1,7 @@
-function noteCall () {
-    $.get('/getNote?id=' + document.getElementById("inquiry").value, function(data, status) {
+function noteCall() {
+    let get = document.getElementById("inquiry").selectedIndex
+    //alert(document.getElementsByTagName("option")[get].value)
+    $.get('/getNote?id=' + document.getElementsByTagName("option")[get].value, function (data, status) {
         // alert(data);
         // alert(status);
 
@@ -7,13 +9,64 @@ function noteCall () {
             var html = " ";
 
             html += "<br /><h3>The following note is: </h3> <br /> <h4>" + data.noteentry + "</h4><br />";
-            html += "<h3>This will be due on: </h3><br /><h4>" + data.duedate + "</h4>";
+            html += "<h3>This is to be done by: </h3><br /><h4>" + data.duedate.split("T")[0] + "</h4><br /><br />";
 
             document.getElementById("remind").innerHTML = html;
 
-        } 
-        else {
+        } else {
             alert("Does not excist")
         }
     })
+}
+
+function locatetodo() {
+    $.get('/locatetodo', function (data, status) {
+        let html = " ";
+
+        html += "<select id='inquiry'>"
+        for (var i = 0; i < data.length; i++) {
+            html += "<option id=" + data[i].id + ">" + data[i].id + "</option>"
+        }
+        html += "</select>"
+
+        document.getElementById("locate").innerHTML = html;
+
+    })
+}
+//Below is Milestone 2
+function addNote() {
+    $.post('/addNote', {duedate:document.getElementById("duedate").value, noteentry:document.getElementById("noteentry").value}, function (data, status) {
+        data[i].id
+    })
+    location.reload();
+}
+
+function updateNoteRequest() {
+    let change = " ";
+    
+    change += "<h5>Change notes content:<br /></h5>";
+    change += '<input type="text" name="noteentry" id="noteentry"><br /><br />'
+    change += "<input type='submit' value='Complete Update' class='btn btn-outline-success' onclick='updateNote()'>";
+
+    document.getElementById("allowChange").innerHTML = change;    
+}
+
+function updateNote() {
+    $.post('/updateNote', {noteentry:document.getElementById("noteentry").value}, function (data, status) {
+        data[i].id
+    })
+    location.reload();
+}
+
+function deleteNote() { // being worked on
+    let get = document.getElementById("inquiry").selectedIndex;
+
+    alert("Your note is being deleted")
+
+    $.get('/deleteNote?id=' + document.getElementsByTagName("option")[get].value, function (data, status) {
+        
+    })
+    location.reload();
+
+    
 }
